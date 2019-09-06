2f07055d5e524158ccf0f48c285b0fef52870c0d
Adebimpe Adebowale <adeadebimpe96@gmail.com>
2019-09-06 15:06:08 +0100

    "added site"

---
+++ gatsby-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteMetadata: {
+    title: `Pennysmart Community`,
+    description: `We are students, working professionals, business founders, and local traders; our community represents a fine mix of amazing individuals. We host members to amazing events and we invite you to join us on this wonderful journey to groom the next wealthy generation of Africans.`,
+    author: `@adeadebimpe`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-react-svg`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import Logo from '../images/Pennysmart-white.svg';
+
+import './index.css';
+import SEO from '../components/seo';
+
+const IndexPage = () => (
+	<div className="community">
+		<div className="row">
+			<div class="col">
+				<div className="community__image">
+          <Logo />
+        </div>
+			</div>
+			<div class="col">
+				<div className="community__text">
+					<h1>
+						Meet other <span>financially savvy</span> people in Accra.
+					</h1>
+					<p>
+						We are students, working professionals, business founders, and local traders; our community
+						represents a fine mix of amazing individuals. We host members to amazing events and we invite
+						you to join us on this wonderful journey to groom the next wealthy generation of Africans. </p>
+              <p> We
+						host events on savings and investment approaches, building and creating wealth, and improvement
+						or addition of new skill sets. </p>
+            <p> Our Team focuses on choosing carefully curated finance advice and
+						experts, determining new ways to help you increase your income and improving your employability
+						or promotion at work. </p>
+            <button class="btn--primary">
+              Join our community
+            </button>
+				</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default IndexPage;
