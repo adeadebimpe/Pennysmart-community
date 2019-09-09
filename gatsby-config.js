1f79498cd12e93169c97f732d2030cf0c1a393d4
Adebimpe Adebowale <adebimpe@pennysmartapp.com>
2019-09-09 12:46:42 +0100

    edoted gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
 				short_name: `starter`,
 				start_url: `/`,
 				display: `minimal-ui`,
-				icon: `src/images/favicon.png` // This path is relative to the root of the site.
+				icon: `src/images/Favicon.png` // This path is relative to the root of the site.
 			}
 		}
 
